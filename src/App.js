0e7c38d299848a1386f776e4c7c7bd1917f9ffbb
Bilal <bsajid173@gmail.com>
2022-05-25 22:10:39 +0530

    Add routes and pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
 function App() {
   return (
     <div>
-
+      <Routes>
+        <Route path="/quotes" element={<AllQuotes />} />
+        <Route path="/quotes/:quoteId" element={<QuoteDetail />} />
+        <Route path="/new-quote" element={<NewQuote />} />
+      </Routes>
     </div>
   );
 }
