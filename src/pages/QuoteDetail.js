35b57a7295134332205f12107c6b6aac8378c246
Bilal <bsajid173@gmail.com>
2022-05-26 21:12:12 +0530

    Output Data on Details Page

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,10 +1,20 @@
 import { Outlet, useParams } from "react-router-dom";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "Maximilian", text: "Learning React is great!" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  if (!quote) {
+    return <p>No Quote Found!!</p>;
+  }
   return (
     <div>
-      <h1>{params.quoteId}</h1>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Outlet />
     </div>
   );
