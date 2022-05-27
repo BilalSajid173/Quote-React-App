68dbb23df6239434358eabad596f0c62db2c6343
Bilal <bsajid173@gmail.com>
2022-05-27 13:48:50 +0530

    connect to comments and some other changes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,4 +1,5 @@
-import { Outlet, useParams } from "react-router-dom";
+import { Outlet, useParams, Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
@@ -8,13 +9,32 @@ const DUMMY_QUOTES = [
 
 const QuoteDetail = () => {
   const params = useParams();
+  const location = useLocation();
+  const [seeComment, setSeeComment] = useState(false);
+  // console.log(location);
   const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
   if (!quote) {
     return <p>No Quote Found!!</p>;
   }
+
+  //theres a bug here, can be solved by useEffect
+  const commentsHandler = () => {
+    setSeeComment(true);
+  };
+
   return (
     <div>
       <HighlightedQuote text={quote.text} author={quote.author} />
+
+      {!seeComment && (
+        <Link
+          onClick={commentsHandler}
+          className="btn--flat"
+          to={`${location.pathname}/comments`}
+        >
+          Comments
+        </Link>
+      )}
       <Outlet />
     </div>
   );
